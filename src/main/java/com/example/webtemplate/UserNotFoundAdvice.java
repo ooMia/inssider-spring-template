ea3baddbfae94c7fb7974ab6d2b7be957c4560eb
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-05-18 20:01:31 +0900

    ✨ Feat : RFC 9457 ProblemDetail

--- src/main/java/com/example/webtemplate/UserNotFoundAdvice.java
+++ src/main/java/com/example/webtemplate/UserNotFoundAdvice.java
@@ -1,6 +1,7 @@
 package com.example.webtemplate;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +11,11 @@ class UserNotFoundAdvice {
 
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String userNotFoundHandler(UserNotFoundException ex) {
-        return ex.getMessage();
+    public ProblemDetail userNotFoundHandler(UserNotFoundException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problemDetail.setType(Util.buildAbsoluteUri("/error/user-not-found"));
+        problemDetail.setTitle("User Not Found");
+        problemDetail.setDetail(ex.getMessage());
+        return problemDetail;
     }
 }
